cc5faf80ac89a30dd99faf1d3af4a91534d70052
BrunoRobim <50236231+BrunoRobim@users.noreply.github.com>
2022-04-03 15:46:10 -0300

    tradução e adição dos comentários no código

--- receiver.ino
+++ receiver.ino
@@ -1,32 +1,48 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+
 void setup() {
+  // Inicia a porta serial na frequencia de 9600, para que seja
+  // feita a leitura no monitor serial da arduino IDE:
+  // Configurar a frequencia do monitor para a mesma frequência 
+  // iniciada na porta serial
   Serial.begin(9600);
+  
+  // Enquanto o serial estiver iniciado
   while (!Serial);
 
-  Serial.println("LoRa Receiver");
+  // printar a mensagem "lora receiver" no monitor serial
+  // da arduino IDE
+  Serial.println("Receptor LoRa")
 
+  // Se a comunicação LoRa não for iniciada em 915Mhz,
+  // frequência ajustável de operação do protocolo e 
+  // antenas. Imprime a mensagem de erro de falha de 
+  // comunicação.
   if (!LoRa.begin(915E6)) {
-    Serial.println("Starting LoRa failed!");
+    Serial.println("Inicialização da comunicação LoRa falhou!");
     while (1);
   }
 }
 
 void loop() {
-  // try to parse packet
+  // Tenrta converter o pacote recebido via LoRa Connection
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
-    // received a packet
-    Serial.print("Received packet '");
+    // Printa a mensagem de pacote recebido
+    Serial.print("Pacote Recebido '");
 
-    // read packet
+    // Faz a leitura do pacote
     while (LoRa.available()) {
+      // Imprime no Monitor Serial da Arduino IDE o pacote lido
       Serial.print((char)LoRa.read());
     }
 
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
+    // Imprime no Monitor Serial da Arduino IDE o RSSI,
+    // Indicador da força do sinal recebido pelo pacote,
+    // medido em dBm.
+    Serial.print("' com RSSI ");
     Serial.println(LoRa.packetRssi());
   }
 }
